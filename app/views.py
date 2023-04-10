de8a3b528a1b8ab0abd6b4ec82062a25324ccf1b
rajeswarimangala <rajeswarimscm123@gmail.com>
2023-04-10 22:05:04 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def meghana(request):
+    return HttpResponse('my name is meghana sir !!!!!!!!!')
+def srujana(request):
+    return HttpResponse('srujana is good girl and she studies well !')
